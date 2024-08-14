188f45cd5b99e9bba3de8daee530c68c5aed878e
Bruce Estivil <baestivil@gmail.com>
2024-08-15 00:43:35 +0100

    feat: init localforage

--- app/util/datetime.ts
+++ app/util/datetime.ts
@@ -1,14 +1,4 @@
-import { useEffect, useState } from "react";
-
 const d = new Date();
-
-export default function DateTime() {
-  const [dateTime, setDateTime] = useState<string>("");
-
-  setDateTime(dformat);
-
-  return dateTime;
-}
 
 export var dformat =
   [d.getMonth() + 1, d.getDate(), d.getFullYear()].join("/") +
